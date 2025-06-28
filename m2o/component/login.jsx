38286771a8de4dcd72aa79ec82cd45155111bbf1
Lutfiana Deka Nurhayati <125198785+LutfianaDeka@users.noreply.github.com>
2025-06-28 09:37:21 +0700

    edit loginPage

--- m2o/component/login.jsx
+++ m2o/component/login.jsx
@@ -2,7 +2,7 @@ export default function LoginPage() {
   return (
     <>
       <div className="login bg-black h-screen">
-        <h1 className="text-[#fff313] font-bold text-3xl">LOGIN</h1>
+        <h1 className="text-[#fff313] font-bold text-3xl text center">LOGIN</h1>
       </div>
     </>
   );
